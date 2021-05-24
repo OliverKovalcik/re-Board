113581b8916f128761d60abf389aa96e64d93b9a
Oliver Kovalcik <oliver.kovalcik@gmail.com>
2021-05-24 10:26:51 +0200

    Update Board

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,28 +1,90 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, Center } from '@chakra-ui/react'
-// import { getBoard } from '../utils/api'
+import { CloseButton, Box, Input, Button, Center, Grid, VStack, HStack } from '@chakra-ui/react'
+
+import { useFetch } from '../hooks'
+import { Tasks } from '../components/Tasks'
+import { getTaskGroups, createTaskGroup, removeTaskGroup, updateTaskGroup } from '../utils/api'
 
 const Board = () => {
   const { id } = useParams()
-  // const [status, setStatus] = React.useState('loading')
-  // const [board, setBoard] = React.useState({})
+  const [newListName, setNewListName] = React.useState('')
 
-  // useEffect(() => {
-  //   try {
-  //     const fetchData = async () => {
-  //       const data = await getBoard(id)
-  //       setBoard(data)
-  //     }
-  //     fetchData()
-  //   } catch (e) {
-  //     // do nothing
-  //   }
-  // }, [id])
+  const [updateListName, setUpdateListName] = React.useState()
+  const { data, reloadPage } = useFetch(getTaskGroups, id)
+
+  const handleCreateNewTaskList = () => {
+    createTaskGroup(Number(id), newListName).then(() => {
+      setNewListName('')
+      reloadPage()
+    })
+  }
+  // NEFUNGUJE
+  const handleRemoveList = async (currentListId) => {
+    console.log(currentListId)
+    await removeTaskGroup(currentListId)
+    reloadPage()
+  }
+
+  const handleChangeListName = (listId) => {
+    const updatedListName = { name: updateListName }
+    updateTaskGroup(listId, updatedListName)
+  }
 
   return (
-    <Box h="100vh">
-      <Center h="100%">Board {id} detail</Center>
+    <Box>
+      <Center m="5">
+        <Input
+          w="50"
+          variant="outline"
+          placeholder="Add New List"
+          onChange={(e) => setNewListName(e.target.value)}
+          value={newListName}
+        />
+        <Button size="md" onClick={() => handleCreateNewTaskList()}>
+          Create new List
+        </Button>
+      </Center>
+
+      <Grid templateColumns="repeat(20, 1fr)" templaterows="repeat(1, 1fr)" gap={6}>
+        {data &&
+          data.map(({ taskIds, id: currentListId, name }) => {
+            return (
+              <Box key={currentListId} w="250px" maxHeight="770px">
+                <Box
+                  // color={toggleColor ? 'pink.400' : 'white'}
+                  color="white"
+                  m="3"
+                  borderRadius="10"
+                  backgroundColor="blackAlpha.800"
+                  height="12vh"
+                >
+                  <VStack>
+                    <HStack marginLeft="183px">
+                      {/* Close Button */}
+                      <CloseButton onClick={() => handleRemoveList(currentListId)} />
+                    </HStack>
+                    <Center>
+                      <Input
+                        marginLeft="1px"
+                        defaultValue={name}
+                        onBlur={() => handleChangeListName(currentListId)}
+                        focusBorderColor="pink.400"
+                        variant="flushed"
+                        onChange={(e) => setUpdateListName(e.target.value)}
+                      />
+                    </Center>
+                  </VStack>
+                </Box>
+                <Tasks
+                  taskIds={taskIds}
+                  currentListId={currentListId}
+                  reloadComponent={reloadPage}
+                />
+              </Box>
+            )
+          })}
+      </Grid>
     </Box>
   )
 }
