f91b78539e1e33fea6c4504c5f82f23f951ab961
Oliver Kovalcik <oliver.kovalcik@gmail.com>
2021-06-09 19:09:14 +0200

    Fix drag and drop and delete copy tasks

--- src/components/tasks/TaskGroup.jsx
+++ src/components/tasks/TaskGroup.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Grid, Heading, Text, Flex, Spacer } from '@chakra-ui/react'
 import { DragDropContext, Droppable } from 'react-beautiful-dnd'
-
+import PropTypes from 'prop-types'
 import { Tasks, TaskGroupOptions } from '.'
 import { updateTaskGroup } from '../../utils/api'
 
@@ -10,9 +10,14 @@ export const TaskGroups = ({ taskGroups, reloadPage, setTaskGroups, tasks, data 
     const items = Array.from(taskGroups)
     const { source, destination } = result
     if (!result.destination) return
+    // drag and drop between columns
     if (source.droppableId !== destination.droppableId) {
-      const sourceColumn = items[source.droppableId - 1]
-      const destColumn = items[destination.droppableId - 1]
+      const sourceColumnIndex = items.findIndex((x) => x.id === Number(source.droppableId))
+      const destinationColumnIndex = items.findIndex(
+        (x) => x.id === Number(destination.droppableId)
+      )
+      const sourceColumn = items[sourceColumnIndex]
+      const destColumn = items[destinationColumnIndex]
 
       // tu sa vyberame uz konkretne array
       const sourceItems = [...sourceColumn.taskIds]
@@ -21,14 +26,16 @@ export const TaskGroups = ({ taskGroups, reloadPage, setTaskGroups, tasks, data 
       // uz len toto dole poriesit
       const [removed] = sourceItems.splice(source.index, 1)
       destItems.splice(destination.index, 0, removed)
-      const finalSourceChange = { ...items[source.droppableId - 1], taskIds: sourceItems }
+
+      const finalSourceChange = { ...items[sourceColumnIndex], taskIds: sourceItems }
+
       const finalDestinationChange = {
-        ...items[destination.droppableId - 1],
+        ...items[destinationColumnIndex],
         taskIds: destItems,
       }
-      items.splice(source.droppableId - 1, 1, finalSourceChange)
+      items.splice(sourceColumnIndex, 1, finalSourceChange)
 
-      items.splice(destination.droppableId - 1, 1, finalDestinationChange)
+      items.splice(destinationColumnIndex, 1, finalDestinationChange)
 
       const newSomething = { ...data, taskGroups: items }
 
@@ -36,13 +43,25 @@ export const TaskGroups = ({ taskGroups, reloadPage, setTaskGroups, tasks, data 
       updateTaskGroup(destination.droppableId, { taskIds: destItems })
 
       setTaskGroups(newSomething)
-    } else {
-      const column = items[Number(source.droppableId) - 1]
+    }
+    // drag and drop in one column
+    else {
+      const newColumn = items.filter((x) => x.id === Number(source.droppableId))
+
+      const column = newColumn[0]
+
       const copiedItems = [...column.taskIds]
+
       const [removed] = copiedItems.splice(source.index, 1)
+
       copiedItems.splice(destination.index, 0, removed)
+
       const newOrderOfArray = { ...column, taskIds: copiedItems }
-      items.splice(Number(source.droppableId) - 1, 1, newOrderOfArray)
+
+      const indexOfChangedObject = items.findIndex((x) => x.id === Number(source.droppableId))
+
+      items.splice(indexOfChangedObject, 1, newOrderOfArray)
+
       const newSomething = { ...data, taskGroups: items }
       updateTaskGroup(source.droppableId, { taskIds: copiedItems })
       setTaskGroups(newSomething)
@@ -51,60 +70,67 @@ export const TaskGroups = ({ taskGroups, reloadPage, setTaskGroups, tasks, data 
   return (
     <>
       <DragDropContext onDragEnd={handleDragEnd}>
-        {taskGroups &&
-          taskGroups.map(({ taskIds, id: currentListId, name, boardId, color }) => {
-            return (
-              <Droppable key={currentListId} droppableId={currentListId.toString()}>
-                {(provided) => (
-                  <ul {...provided.droppableProps} ref={provided.innerRef}>
-                    <Grid
-                      key={currentListId}
-                      borderRadius="md"
-                      boxShadow="md"
-                      w="300px"
-                      margin="10px"
-                      background={color}
-                      textAlign="center"
-                      overflow="clip"
-                    >
-                      <Heading m="2">
-                        <Flex h="10px" p="2">
-                          <Spacer />
-                          <TaskGroupOptions
-                            groupId={currentListId}
-                            tasksIds={taskIds}
-                            groupName={name}
-                            reloadPage={reloadPage}
-                            activeGroupColor={color}
-                          />
-                        </Flex>
-                        <Text
-                          fontWeight={600}
-                          fontSize={{ base: '2xl', sm: '4xl', md: '3xl' }}
-                          lineHeight="110%"
-                        >
-                          {name}
-                        </Text>
-                      </Heading>
+        {taskGroups?.map(({ taskIds, id: currentListId, name, boardId, color }) => {
+          return (
+            <Droppable key={currentListId} droppableId={currentListId.toString()}>
+              {(provided) => (
+                <ul {...provided.droppableProps} ref={provided.innerRef}>
+                  <Grid
+                    key={currentListId}
+                    borderRadius="md"
+                    boxShadow="md"
+                    w="300px"
+                    margin="10px"
+                    background={color}
+                    textAlign="center"
+                    overflow="clip"
+                  >
+                    <Heading m="2">
+                      <Flex h="10px" p="2">
+                        <Spacer />
+                        <TaskGroupOptions
+                          groupId={currentListId}
+                          tasksIds={taskIds}
+                          groupName={name}
+                          reloadPage={reloadPage}
+                          activeGroupColor={color}
+                        />
+                      </Flex>
+                      <Text
+                        fontWeight={600}
+                        fontSize={{ base: '2xl', sm: '4xl', md: '3xl' }}
+                        lineHeight="110%"
+                      >
+                        {name}
+                      </Text>
+                    </Heading>
 
-                      <Tasks
-                        tasks={tasks}
-                        provided={provided}
-                        setTaskGroups={setTaskGroups}
-                        data={data}
-                        taskIds={taskIds}
-                        currentListId={currentListId}
-                        reloadComponent={reloadPage}
-                        boardId={boardId}
-                        taskGroups={taskGroups}
-                      />
-                    </Grid>
-                  </ul>
-                )}
-              </Droppable>
-            )
-          })}
+                    <Tasks
+                      tasks={tasks}
+                      provided={provided}
+                      setTaskGroups={setTaskGroups}
+                      data={data}
+                      taskIds={taskIds}
+                      currentListId={currentListId}
+                      reloadComponent={reloadPage}
+                      boardId={boardId}
+                      taskGroups={taskGroups}
+                    />
+                  </Grid>
+                </ul>
+              )}
+            </Droppable>
+          )
+        })}
       </DragDropContext>
     </>
   )
+}
+
+TaskGroups.propTypes = {
+  taskGroups: PropTypes.arrayOf(),
+  reloadPage: PropTypes.func,
+  setTaskGroups: PropTypes.func,
+  tasks: PropTypes.arrayOf(),
+  data: PropTypes.arrayOf(),
 }
--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,154 +1,101 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
-import {
-  Box,
-  Button,
-  Center,
-  CloseButton,
-  Stack,
-  Textarea,
-  Input,
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogContent,
-  AlertDialogOverlay,
-  Select,
-} from '@chakra-ui/react'
-import { CopyIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+import { Box, Button, Center, CloseButton, Stack, Textarea, Input } from '@chakra-ui/react'
+import { Draggable } from 'react-beautiful-dnd'
 import PropTypes from 'prop-types'
-import { useFetch } from '../../hooks'
-import {
-  getTasks,
-  createTask,
-  removeTask,
-  updateTask,
-  getTaskGroups,
-  updateTaskGroup,
-} from '../../utils/api'
+import { createTask, removeTask, updateTask } from '../../utils/api'
 
-export const Tasks = ({ taskIds, currentListId, reloadComponent, taskGroups }) => {
+export const Tasks = ({
+  taskIds,
+  currentListId,
+  reloadComponent,
+  provided: providedPlaceholder,
+  boardId,
+  tasks,
+}) => {
   const { id } = useParams()
-  const { data, reloadPage } = useFetch(getTasks, id)
   const [taskContent, setTaskContent] = React.useState()
   const [taskName, setTaskName] = React.useState()
-  const [targetGroupId, setTargetGroupId] = React.useState()
+
+  // create new task
   const handleCreateNewTask = async () => {
     await createTask(Number(id), currentListId)
-    await reloadPage()
     await reloadComponent()
   }
-  const handleRemoveTask = async (boardId, taskID) => {
-    removeTask(boardId, taskID)
-    await reloadPage()
-    await reloadComponent()
+  // remove task
+  const handleRemoveTask = (idOfBoard, taskID) => {
+    removeTask(idOfBoard, taskID)
+    reloadComponent()
   }
-  // Update task
-  const handleSaveTaskContent = async (taskID, tasks, value) => {
-    const newContent = { content: value }
-    await updateTask(taskID, { ...tasks, ...newContent })
-    reloadPage()
+  // update task content
+  const handleSaveTaskContent = (taskID, task) => {
+    const newContent = { content: taskContent }
+    updateTask(taskID, { ...task, ...newContent })
   }
-  const handleSaveTaskName = async (taskID, tasks, value) => {
-    const newName = { name: value }
-    await updateTask(taskID, { ...tasks, ...newName })
-    reloadPage()
+  // update task name
+  const handleSaveTaskName = (taskID, task) => {
+    const newName = { name: taskName }
+    updateTask(taskID, { ...task, ...newName })
+    reloadComponent()
   }
-  const handleCopyTask = async (boardId, taskGroupId, task) => {
-    await createTask(boardId, taskGroupId, { ...task, id: null })
-    await reloadPage()
-    await reloadComponent()
-  }
-  const handleMoveTask = async (taskGroupsArray, taskId, listId) => {
-    const taskGroupIds = taskGroupsArray
-      .filter((item) => item.id === listId)
-      .map((item) => item.taskIds)
-    const filteredIds = taskGroupIds[0].filter((idx) => idx !== taskId)
-    const newTaskGroupIds = taskGroupsArray.filter((item) => {
-      return item.id == targetGroupId
-    })
-    const test = newTaskGroupIds.map((item) => item.taskIds)
-    const add = test[0]
-    console.log(add)
-    // delete task from taksGroup
-    await updateTaskGroup(targetGroupId, { taskIds: [...add, taskId] })
-    await updateTaskGroup(listId, { taskIds: filteredIds })
 
-    await reloadPage()
-    await reloadComponent()
-    onClose()
-  }
-  const [isOpen, setIsOpen] = React.useState(false)
-  const onClose = () => setIsOpen(false)
-  const cancelRef = React.useRef()
+  const results = []
+  let filtered = tasks?.filter((x) => taskIds?.includes(x.id))
+
+  taskIds.forEach((key) => {
+    let found = false
+
+    // eslint-disable-next-line func-names
+    filtered = filtered.filter(function (item) {
+      if (!found && item.id === key) {
+        results.push(item)
+        found = true
+        return false
+      }
+      return true
+    })
+  })
+
   return (
     <>
-      {taskIds &&
-        data
-          .filter((tasks) => {
-            return taskIds.includes(tasks.id)
-          })
-          .map((tasks) => {
-            return (
-              <div key={tasks.id}>
-                <Box backgroundColor="green.200" p="2" m="3" borderRadius="10">
+      {results &&
+        results?.map((x, index) => {
+          return (
+            <Draggable key={x.id} draggableId={x.id.toString()} index={index}>
+              {(provided) => (
+                <Box
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  backgroundColor="green.200"
+                  p="2"
+                  m="3"
+                  borderRadius="10"
+                >
                   <Stack direction="row-reverse" m="5px" spacing={5}>
-                    <CloseButton
-                      boxSize={4}
-                      onClick={() => handleRemoveTask(tasks.boardId, tasks.id)}
-                    />
-                    <CopyIcon onClick={() => handleCopyTask(tasks.boardId, currentListId, tasks)} />
-                    <ArrowForwardIcon onClick={() => setIsOpen(true)} />
-                    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-                      <AlertDialogOverlay>
-                        <AlertDialogContent>
-                          <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                            Move to list:
-                          </AlertDialogHeader>
-                          <AlertDialogBody>
-                            <Select onChange={(event) => setTargetGroupId(event.target.value)}>
-                              {taskGroups.map((item) => {
-                                return (
-                                  <option value={item.id} key={item.id}>
-                                    {item.name}
-                                  </option>
-                                )
-                              })}
-                            </Select>
-                          </AlertDialogBody>
-                          <AlertDialogFooter>
-                            <Button ref={cancelRef} onClick={onClose}>
-                              Cancel
-                            </Button>
-                            <Button
-                              colorScheme="blue"
-                              onClick={() => handleMoveTask(taskGroups, tasks.id, currentListId)}
-                              ml={3}
-                            >
-                              Move
-                            </Button>
-                          </AlertDialogFooter>
-                        </AlertDialogContent>
-                      </AlertDialogOverlay>
-                    </AlertDialog>
+                    <CloseButton boxSize={4} onClick={() => handleRemoveTask(boardId, x.id)} />
+
                     <Input
                       variant="unstyled"
-                      onChange={(e) => handleSaveTaskName(tasks.id, tasks, e.target.value)}
+                      onBlur={() => handleSaveTaskName(x.id, x)}
+                      onChange={(e) => setTaskName(e.target.value)}
                       placeholder="Title"
-                      defaultValue={tasks.name}
+                      defaultValue={results[index].name}
                     />
                   </Stack>
                   <Textarea
-                    onChange={(e) => handleSaveTaskContent(tasks.id, tasks, e.target.value)}
+                    onBlur={() => handleSaveTaskContent(x.id, x)}
+                    onChange={(e) => setTaskContent(e.target.value)}
                     placeholder="Write here"
                     focusBorderColor="pink.400"
-                    defaultValue={tasks.content}
+                    defaultValue={results[index].content}
                   />
                 </Box>
-              </div>
-            )
-          })}
+              )}
+            </Draggable>
+          )
+        })}
+      {providedPlaceholder.placeholder}
       {/* Tasks */}
       <Center>
         <Button onClick={() => handleCreateNewTask()} size="sm" m="2">
@@ -162,4 +109,7 @@ Tasks.propTypes = {
   taskIds: PropTypes.arrayOf(PropTypes.number),
   currentListId: PropTypes.number,
   reloadComponent: PropTypes.func,
+  provided: PropTypes.objectOf(),
+  boardId: PropTypes.number,
+  tasks: PropTypes.objectOf(),
 }
